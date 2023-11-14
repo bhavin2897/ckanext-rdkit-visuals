e6ebaa3a7a2c545b9c4d46d377697b4ac1057dfa
Bhavin Katabathuni <bhavin2897@gmail.com>
2023-11-14 13:53:30 +0100

    Addition of molecule package relationship table

--- ckanext/rdkit_visuals/migration/rdkit_visuals/versions/2e23c588066e_rename_molecule_data_table_title.py
+++ ckanext/rdkit_visuals/migration/rdkit_visuals/versions/2e23c588066e_rename_molecule_data_table_title.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '2e23c588066e'
-down_revision = '51fd43f24eaf'
+down_revision =  'ccd38ad5fced'  #'51fd43f24eaf' This is temporary, will be removed soon.
 branch_labels = None
 depends_on = None
 
